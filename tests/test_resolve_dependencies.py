0c7648d7b15fcfdf4465d3622e0a2b38291140c6
Vladyslav Timofeev <Timofeevvladyslav49@gmail.com>
2023-12-16 17:09:31 +0000

    add call order to test_resolve_dependencies

--- tests/test_resolve_dependencies.py
+++ tests/test_resolve_dependencies.py
@@ -1,19 +1,18 @@
-import time
+from collections.abc import MutableSequence
 from contextlib import AsyncExitStack
 from typing import Annotated
 
 import pytest
 from aiogram import Dispatcher
 from aiogram.dispatcher.event.handler import HandlerObject
-from aiogram.enums import ChatType
-from aiogram.types import Message, Chat, User
+from aiogram.types import Message, User, TelegramObject
 
 from aiogram3_di import Depends, setup_di
 from aiogram3_di.resolver import DependenciesResolver
 
 
-def get_user_full_name(event: Message) -> str:
-    return event.from_user.full_name
+def get_user_full_name(event_from_user: User) -> str:
+    return event_from_user.full_name
 
 
 async def start(
@@ -22,18 +21,30 @@ async def start(
     await message.answer(f"Hi {full_name}")
 
 
+class MockedDependency:
+    __slots__ = ("_call_args_list", "_return_value")
+
+    def __init__(
+        self, call_args_list: MutableSequence[str], *, return_value: str | None = None
+    ) -> None:
+        self._call_args_list = call_args_list
+        self._return_value = return_value
+
+    def __call__(self) -> str | None:
+        self._call_args_list.append(self._return_value)
+        return self._return_value
+
+
 @pytest.mark.asyncio
 async def test_resolve_dependencies(dp: Dispatcher) -> None:
-    setup_di(dp)
-    handler_dependencies = ()
-    event = Message(
-        message_id=0,
-        date=time.time(),
-        chat=Chat(id=0, type=ChatType.PRIVATE),
-        from_user=User(
-            id=0, is_bot=False, first_name="Vladyslav", last_name="Timofeev"
-        ),
+    call_args_list = []
+    mocked_verify_user = MockedDependency(call_args_list)
+    mocked_get_user_full_name = MockedDependency(
+        call_args_list, return_value="Vladyslav Timofeev"
     )
+    setup_di(dp, dependency_overrides={get_user_full_name: mocked_get_user_full_name})
+    handler_dependencies = (Depends(mocked_verify_user),)
+    event = TelegramObject()
     middleware_data = dp.workflow_data | {"handler": HandlerObject(start)}
 
     async with AsyncExitStack() as stack:
@@ -45,4 +56,7 @@ async def test_resolve_dependencies(dp: Dispatcher) -> None:
         )
         middleware_data = await resolver.resolve()
 
+    assert len(call_args_list) == 2
+    assert call_args_list[0] is None
+    assert call_args_list[1] == "Vladyslav Timofeev"
     assert middleware_data["full_name"] == "Vladyslav Timofeev"
